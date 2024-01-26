1237f3e88c83661825825a7c9f76dc0143a0b01e
nkolis <nurkholis010@gmail.com>
2024-01-26 08:52:05 +0700

    update

--- views/resep/hapus_resep.php
+++ views/resep/hapus_resep.php
@@ -1,18 +1,18 @@
 <?php
 include '../koneksi.php';
-$kd_periksa = $_GET['kd_periksa'];
-$data = mysqli_query($conn, "DELETE FROM tb_periksa WHERE kd_periksa='$kd_periksa' ");
+$kd_resep = $_GET['kd_resep'];
+$data = mysqli_query($conn, "DELETE FROM tb_periksa WHERE kd_periksa='$kd_resep' ");
 
 if ($data) {
   echo "
                       <script language=javascript>
                         alert('Data berhasil dihapus');
-                        document.location.href='?hal=data_periksa';
+                        document.location.href='?hal=data_resep';
                       </script>";
 } else {
   echo "
                         <script language=javascript>
                           alert('Data gagal dihapus');
-                          document.location.href='?hal=data_periksa'; 
+                          document.location.href='?hal=data_resep'; 
                         </script>";
 }
